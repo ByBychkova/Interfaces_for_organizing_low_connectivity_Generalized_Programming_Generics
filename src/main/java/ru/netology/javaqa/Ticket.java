0ed1e8369b30fd46d9068d03ae134a2fd0e95afb
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-10-05 11:32:50 +0300

    Finish commit2

--- src/main/java/ru/netology/javaqa/Ticket.java
+++ src/main/java/ru/netology/javaqa/Ticket.java
@@ -1,65 +1,67 @@
 package ru.netology.javaqa;
+
 import java.util.Objects;
-    public class Ticket implements Comparable<Ticket> {
-        private String from; // аэропорт откуда
-        private String to; // аэропорт куда
-        private int price; // цена
-        private int timeFrom; // время вылета (по москве)
-        private int timeTo; // время прилёта (по москве)
 
-        public Ticket(String from, String to, int price, int timeFrom, int timeTo) {
-            this.from = from;
-            this.to = to;
-            this.price = price;
-            this.timeFrom = timeFrom;
-            this.timeTo = timeTo;
-        }
+public class Ticket implements Comparable<Ticket> {
+    private String from; // аэропорт откуда
+    private String to; // аэропорт куда
+    private int price; // цена
+    private int timeFrom; // время вылета (по москве)
+    private int timeTo; // время прилёта (по москве)
 
-        public String getFrom() {
-            return from;
-        }
+    public Ticket(String from, String to, int price, int timeFrom, int timeTo) {
+        this.from = from;
+        this.to = to;
+        this.price = price;
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
+    }
 
-        public String getTo() {
-            return to;
-        }
+    public String getFrom() {
+        return from;
+    }
 
-        public int getPrice() {
-            return price;
-        }
+    public String getTo() {
+        return to;
+    }
 
-        public int getTimeFrom() {
-            return timeFrom;
-        }
+    public int getPrice() {
+        return price;
+    }
 
-        public int getTimeTo() {
-            return timeTo;
-        }
+    public int getTimeFrom() {
+        return timeFrom;
+    }
+
+    public int getTimeTo() {
+        return timeTo;
+    }
 
 
-        // Вспомогательные методы для корректной работы equals
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Ticket ticket = (Ticket) o;
-            return price == ticket.price && timeFrom == ticket.timeFrom && timeTo == ticket.timeTo && from.equals(ticket.from) && to.equals(ticket.to);
-        }
+    // Вспомогательные методы для корректной работы equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return price == ticket.price && timeFrom == ticket.timeFrom && timeTo == ticket.timeTo && from.equals(ticket.from) && to.equals(ticket.to);
+    }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(from, to, price, timeFrom, timeTo);
-        }
+    @Override
+    public int hashCode() {
+        return Objects.hash(from, to, price, timeFrom, timeTo);
+    }
 
-        @Override
-        public int compareTo(Ticket o) {
-            if (this.price < o.price){
+    @Override
+    public int compareTo(Ticket o) {
+        if (this.price < o.price) {
             return -1;
-        } else if (this.price> o.price)  {
-                return 1;
-            }else {
-                return 0;
-            }
-
+        } else if (this.price > o.price) {
+            return 1;
+        } else {
+            return 0;
         }
+
+    }
 
 }
--- src/test/java/ru/netology/javaqa/AviaSoulsTest.java
+++ src/test/java/ru/netology/javaqa/AviaSoulsTest.java
@@ -11,38 +11,39 @@ class AviaSoulsTest {
     @Test
     public void testSortTickets() {
         AviaSouls manager = new AviaSouls();
-        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 12,14);
-        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18,20);
-        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14,16);
-        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13,15);
-        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12,24);
+        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 12, 14);
+        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20);
+        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14, 16);
+        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13, 15);
+        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12, 24);
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
 
-       Ticket [] actual = manager.search("MSK", "NN");
-        Ticket [] expected ={ticket2, ticket4, ticket1};
+        Ticket[] actual = manager.search("MSK", "NN");
+        Ticket[] expected = {ticket2, ticket4, ticket1};
         Assertions.assertArrayEquals(expected, actual);
 
     }
+
     @Test
     public void testSortTickets_zero_tickets() {
         AviaSouls manager = new AviaSouls();
-        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 12,14);
-        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18,20);
-        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14,16);
-        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13,15);
-        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12,24);
+        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 12, 14);
+        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20);
+        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14, 16);
+        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13, 15);
+        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12, 24);
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
 
-        Ticket [] actual = manager.search("MSK", "NY");
-        Ticket [] expected ={};
+        Ticket[] actual = manager.search("MSK", "NY");
+        Ticket[] expected = {};
         Assertions.assertArrayEquals(expected, actual);
 
     }
@@ -50,78 +51,82 @@ class AviaSoulsTest {
     @Test
     public void testSortTickets_one_tickets() {
         AviaSouls manager = new AviaSouls();
-        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 12,14);
-        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18,20);
-        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14,16);
-        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13,15);
-        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12,24);
+        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 12, 14);
+        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20);
+        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14, 16);
+        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13, 15);
+        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12, 24);
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
 
-        Ticket [] actual = manager.search("NN", "LA");
-        Ticket [] expected ={ticket5};
+        Ticket[] actual = manager.search("NN", "LA");
+        Ticket[] expected = {ticket5};
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void testSortTickets_same_time() {
         AviaSouls manager = new AviaSouls();
-        Ticket ticket1 = new Ticket("MSK", "NN", 3000, 12,14);
-        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18,20);
-        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14,16);
-        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13,15);
-        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12,24);
+        Ticket ticket1 = new Ticket("MSK", "NN", 3000, 12, 14);
+        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20);
+        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14, 16);
+        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13, 15);
+        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12, 24);
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
 
-        Ticket [] actual = manager.search("MSK", "NN");
-        Ticket [] expected ={ticket1,ticket2,ticket4};
+        Ticket[] actual = manager.search("MSK", "NN");
+        Ticket[] expected = {ticket1, ticket2, ticket4};
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void testTimeTicket(){
-    AviaSouls manager = new AviaSouls();
-    Ticket ticket1 = new Ticket("MSK", "NN", 30000, 11,14); //3
-    Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18,20); //2
-    Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14,16);
-    Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13,14);//1
-    Ticket ticket5 = new Ticket("NN", "LA", 25000, 12,24);
-    manager.add(ticket1);
-    manager.add(ticket2);
-    manager.add(ticket3);
-    manager.add(ticket4);
-    manager.add(ticket5);
+    public void testTimeTicket() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 11, 14); //3
+        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20); //2
+        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14, 16);
+        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13, 14);//1
+        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12, 24);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
 
-    Comparator <Ticket> comparator = new TicketTimeComparator();
-    Ticket [] actual = manager.searchAndSortBy("MSK", "NN", comparator);
-    Ticket [] expected = {ticket4, ticket2, ticket1};
-    Assertions.assertArrayEquals(expected, actual);
+        Comparator<Ticket> comparator = new TicketTimeComparator();
+        Ticket[] actual = manager.searchAndSortBy("MSK", "NN", comparator);
+        Ticket[] expected = {ticket4, ticket2, ticket1};
+        Assertions.assertArrayEquals(expected, actual);
 
-}
-@Test
-public void testTimeTicket_zero_tickets() {
-    AviaSouls manager = new AviaSouls();
-    Ticket ticket1 = new Ticket("MSK", "NN", 30000, 11, 14); //3
-    Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20); //2
-    Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14, 16);
-    Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13, 14);//1
-    Ticket ticket5 = new Ticket("NN", "LA", 25000, 12, 24);
-    manager.add(ticket1);
-    manager.add(ticket2);
-    manager.add(ticket3);
-    manager.add(ticket4);
-    manager.add(ticket5);
+    }
 
-    Comparator<Ticket> comparator = new TicketTimeComparator();
-    Ticket[] actual = manager.searchAndSortBy("MSK", "NY", comparator);
-    Ticket[] expected = {};
-    Assertions.assertArrayEquals(expected, actual);
-}
+    @Test
+    public void testTimeTicket_zero_tickets() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("MSK", "NN", 30000, 11, 14); //3
+        Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20); //2
+        Ticket ticket3 = new Ticket("NN", "MSK", 2000, 14, 16);
+        Ticket ticket4 = new Ticket("MSK", "NN", 15000, 13, 14);//1
+        Ticket ticket5 = new Ticket("NN", "LA", 25000, 12, 24);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+
+        Comparator<Ticket> comparator = new TicketTimeComparator();
+        Ticket[] actual = manager.searchAndSortBy("MSK", "NY", comparator);
+        Ticket[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
     @Test
     public void testTimeTicket_same_time() {
         AviaSouls manager = new AviaSouls();
@@ -138,11 +143,12 @@ public void testTimeTicket_zero_tickets() {
 
         Comparator<Ticket> comparator = new TicketTimeComparator();
         Ticket[] actual = manager.searchAndSortBy("MSK", "NN", comparator);
-        Ticket[] expected = {ticket4,ticket1, ticket2};
+        Ticket[] expected = {ticket4, ticket1, ticket2};
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void testTimeTicket_one_ticket(){
+    public void testTimeTicket_one_ticket() {
         AviaSouls manager = new AviaSouls();
         Ticket ticket1 = new Ticket("MSK", "NN", 30000, 12, 14);
         Ticket ticket2 = new Ticket("MSK", "NN", 3000, 18, 20);
